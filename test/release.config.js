0c2f6a529081b567a69e7622a059a80c0b2d6305
maxim.ulshin <maxim.ulshin@cyper.ca>
2022-08-09 22:39:37 +0300

    fix: fix description

---
+++ test/release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: 'master',
+  repositoryUrl: 'https://github.com/70-6C-65-61-73-75-72-65h/nest-gh-check',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+  ],
+};
