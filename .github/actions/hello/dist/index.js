fa31f96e6070d366e91edd16382c081b6bbfc4f6
maxim.ulshin <maxim.ulshin@cyper.ca>
2022-08-18 14:47:56 +0300

    fix: 1

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -1193,14 +1193,19 @@ const github = __webpack_require__(469);
 
 try {
   const name = core.getInput('who-to-greet');
-
-  console.log(`Hello ${name}`);
+  core.setSecret(name);
+  // console.log(`Hello ${name}`);
+  core.debug(`Hello ${name}`);
+  core.warning(`Hello ${name}`);
 
   const time = new Date();
 
   core.setOutput('time', time.toTimeString());
 
+  core.startGroup('Loggin github object');
   console.log(JSON.stringify(github, null, 2));
+  core.endGroup();
+  core.exportVariable('Hello', 'hello');
 } catch (error) {
   core.setFailed(`if error occured you may see that, ${error.message}`);
 }
